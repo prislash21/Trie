81fe72d5c781785e47760a50c4e9f8c0b064846d
Priota Roy <62557187+prislash21@users.noreply.github.com>
2020-03-25 19:44:29 +0600

    Update TrieNode.java

--- TrieNode.java
+++ TrieNode.java
@@ -1,6 +1,9 @@
 import java.util.HashMap;
-
+       
 public class TrieNode {
+	 /**
+	 *Declaring Variable and a map to hold children
+         */
 	private char c;
 	private HashMap<Character, TrieNode> children = new HashMap<>();
 	private boolean isLeaf;
@@ -8,18 +11,26 @@ public class TrieNode {
 	public TrieNode() {}
 
 	public TrieNode(char c){
+		 /**
+	        *Declaring character to hold the current character
+                 */
 		this.c = c;
 	}
 
 	public HashMap<Character, TrieNode> getChildren() {
 		return children;
-	}
-
+	}    
+           /**
+	        *set a map for holding multiple characters
+                 */
 	public void setChildren(HashMap<Character, TrieNode> children) {
 		this.children = children;
 	}
 
 	public boolean isLeaf() {
+		/**
+	        *set for marking the end node
+                 */
 		return isLeaf;
 	}
 
